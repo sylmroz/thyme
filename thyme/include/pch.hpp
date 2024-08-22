6e3f3d8d30f9ceb0e3ab299ce7e47c631ae2f468
sylwester.mroz <sylwester.mroz@leneldev.com>
2024-08-22 07:03:13 +0200

    create precompiled header pch.hpp

---
+++ thyme/include/pch.hpp
@@ -0,0 +1,5 @@
+#pragma once
+// precompiled header
+
+// stl headers
+#include <iostream>
--- thyme/src/lib.cpp
+++ thyme/src/lib.cpp
@@ -1,5 +1,7 @@
 #include <lib.hpp>
+#include <pch.hpp>
 
 double thyme::getPi() {
+    std::cout << "getting PI!\n";
     return 3.14;
 }
