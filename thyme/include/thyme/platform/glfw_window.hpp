086b78754da54c4089e0ad1e64bd2aee79838a25
sylwester.mroz <sylwester.mroz@leneldev.com>
2024-09-06 06:21:00 +0200

    - reorganize glfw window class - make glfw class context injectable

--- thyme/include/thyme/platform/glfw_window.hpp
+++ thyme/include/thyme/platform/glfw_window.hpp
@@ -1,22 +1,91 @@
 #pragma once
 
+#include "thyme/core/logger.hpp"
 #include "thyme/core/window.hpp"
 
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
 
 #include <memory>
 
 namespace Thyme {
 
-class GlfwWindow : public Window {
+class EmptyContext {
+public:
+    void init(){};
+};
+
+class GlfwVulkanContext {
+public:
+    void init() {
+        if (glfwVulkanSupported() == GLFW_FALSE) {
+            auto message = "GLFW3 does not support vulkan!";
+            TH_API_LOG_ERROR(message);
+            glfwTerminate();
+            throw std::runtime_error(message);
+        }
+    }
+
+    std::vector<std::string> getRequiredInstanceExtensions() {
+        uint32_t instanceExtensionCount{ 0 };
+        auto instanceExtensionBuffer = glfwGetRequiredInstanceExtensions(&instanceExtensionCount);
+        std::vector<std::string> instanceExtension;
+        instanceExtension.reserve(instanceExtensionCount);
+        for (uint32_t i{ 0 }; i < instanceExtensionCount; ++i) {
+            instanceExtension.emplace_back(instanceExtensionBuffer[i]);
+        }
+        return instanceExtension;
+    }
+};
+
+template<typename Context = EmptyContext>
+class GlfwWindow
+    : public Window
+    , public Context {
     using WindowHWND = std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>>;
+
 public:
     GlfwWindow(const WindowConfiguration& config);
-    virtual void poolEvents() override;
-    virtual bool shouldClose() override;
+    virtual void poolEvents() override {
+        glfwPollEvents();
+    }
+    virtual bool shouldClose() override {
+        return glfwWindowShouldClose(m_window.get());
+    }
 
-private:
+    ~GlfwWindow() {
+        glfwTerminate();
+    }
+
     WindowHWND m_window;
 };
 
+
+template<typename Context>
+GlfwWindow<Context>::GlfwWindow(const WindowConfiguration& config) : Thyme::Window{ config } {
+    TH_API_LOG_DEBUG("Create window with parameters: width = {}, height = {}, name = {}",
+                     config.width,
+                     config.height,
+                     config.name);
+    if (glfwInit() == GLFW_FALSE) {
+        auto message = "Failed to initialize GLFW!";
+        TH_API_LOG_ERROR(message);
+        glfwTerminate();
+        throw std::runtime_error(message);
+    }
+    Context::init();
+    m_window = WindowHWND(glfwCreateWindow(config.width, config.height, config.name.c_str(), nullptr, nullptr),
+                          [config](GLFWwindow* window) {
+                              TH_API_LOG_DEBUG("destroying window with parameters: width = {}, height = {}, name = {}",
+                                               config.width,
+                                               config.height,
+                                               config.name)
+                              if (window != nullptr) {
+                                  glfwDestroyWindow(window);
+                              }
+                          });
 };
+
+using VulkanGlfwWindow = GlfwWindow<GlfwVulkanContext>;
+
+};// namespace Thyme
--- thyme/src/core/engine.cpp
+++ thyme/src/core/engine.cpp
@@ -12,27 +12,18 @@
 
 void Thyme::Engine::run() {
     TH_API_LOG_INFO("Start Thyme engine");
-
-    glfwInit();
-    if (glfwVulkanSupported() == GLFW_FALSE) {
-        auto message = "GLFW3 does not support vulkan!";
-        TH_API_LOG_ERROR(message);
-        glfwTerminate();
-        throw std::runtime_error(message);
-    }
+    VulkanGlfwWindow window(WindowConfiguration{ .width = 1280, .height = 920, .name = "Thyme App" });
 
     std::vector<const char*> instanceLayers;
     instanceLayers.emplace_back("VK_LAYER_KHRONOS_validation");
     std::vector<const char*> instanceExtension;
     instanceExtension.emplace_back(vk::EXTDebugReportExtensionName);
+
+    auto glfwInstanceExtension = window.getRequiredInstanceExtensions();
     std::vector<const char*> deviceExtension;
     deviceExtension.emplace_back(vk::KHRSwapchainExtensionName);
 
-    uint32_t instanceExtensionCount{ 0 };
-    auto instanceExtensionBuffer = glfwGetRequiredInstanceExtensions(&instanceExtensionCount);
-    for (uint32_t i{ 0 }; i < instanceExtensionCount; ++i) {
-        instanceExtension.emplace_back(instanceExtensionBuffer[i]);
-    }
+    
 
     vk::ApplicationInfo applicationInfo("Thyme",
                                         vk::makeApiVersion(0, 0, 1, 0),
@@ -48,8 +39,6 @@ void Thyme::Engine::run() {
         TH_API_LOG_ERROR("Failed to create vulkan instance. Message: {}, Code: {}", err.what(), err.code().value());
         throw std::runtime_error("Failed to create vulkan instance.");
     }
-    
-    GlfwWindow window(WindowConfiguration{ .width = 1280, .height = 920, .name = "Thyme App" });
 
     while (!window.shouldClose()) {
         window.poolEvents();
