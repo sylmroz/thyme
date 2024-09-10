ffb714bfb258f83460f3055a19adb0e90de5afff
sylwester.mroz <sylwester.mroz@leneldev.com>
2024-09-10 10:52:11 +0200

    Modify VulkanGlfwWindow window class to creeate more robust solution for create VkSurface

--- thyme/include/thyme/platform/glfw_window.hpp
+++ thyme/include/thyme/platform/glfw_window.hpp
@@ -3,6 +3,7 @@
 #include "thyme/core/logger.hpp"
 #include "thyme/core/window.hpp"
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan.hpp>
 
@@ -10,13 +11,58 @@
 
 namespace Thyme {
 
-class THYME_API EmptyContext {
+template<typename Context = void> class GlfwWindow : public Window {
+    using WindowHWND = std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>>;
+
 public:
-    void init(){};
+    GlfwWindow(const WindowConfiguration& config);
+    virtual void poolEvents() override {
+        glfwPollEvents();
+    }
+    virtual bool shouldClose() override {
+        return glfwWindowShouldClose(m_window.get());
+    }
+
+    ~GlfwWindow() {
+        glfwTerminate();
+    }
+
+private:
+    WindowHWND m_window;
+
+    friend Context;
 };
 
-class THYME_API GlfwVulkanContext {
+
+template<typename Context>
+GlfwWindow<Context>::GlfwWindow(const WindowConfiguration& config) : Thyme::Window{ config } {
+    TH_API_LOG_DEBUG("Create window with parameters: width = {}, height = {}, name = {}",
+                     config.width,
+                     config.height,
+                     config.name);
+    if (glfwInit() == GLFW_FALSE) {
+        auto message = "Failed to initialize GLFW!";
+        TH_API_LOG_ERROR(message);
+        glfwTerminate();
+        throw std::runtime_error(message);
+    }
+    m_window = WindowHWND(glfwCreateWindow(config.width, config.height, config.name.c_str(), nullptr, nullptr),
+                          [config](GLFWwindow* window) {
+                              TH_API_LOG_DEBUG("destroying window with parameters: width = {}, height = {}, name = {}",
+                                               config.width,
+                                               config.height,
+                                               config.name)
+                              if (window != nullptr) {
+                                  glfwDestroyWindow(window);
+                              }
+                          });
+};
+
+class THYME_API VulkanGlfwWindow : public GlfwWindow<VulkanGlfwWindow> {
 public:
+    VulkanGlfwWindow(const WindowConfiguration& config) : GlfwWindow<VulkanGlfwWindow>{ config } {
+        init();
+    }
     void init() {
         if (glfwVulkanSupported() == GLFW_FALSE) {
             auto message = "GLFW3 does not support vulkan!";
@@ -36,56 +82,12 @@ public:
         }
         return instanceExtension;
     }
+
+     VkSurfaceKHR getSurface(vk::Instance instance) {
+         VkSurfaceKHR surface;
+         glfwCreateWindowSurface(instance, this->m_window.get(), nullptr, &surface);
+         return surface;
+     }
 };
-
-template<typename Context = EmptyContext>
-class THYME_API GlfwWindow
-    : public Window
-    , public Context {
-    using WindowHWND = std::unique_ptr<GLFWwindow, std::function<void(GLFWwindow*)>>;
-
-public:
-    GlfwWindow(const WindowConfiguration& config);
-    virtual void poolEvents() override {
-        glfwPollEvents();
-    }
-    virtual bool shouldClose() override {
-        return glfwWindowShouldClose(m_window.get());
-    }
-
-    ~GlfwWindow() {
-        glfwTerminate();
-    }
-
-    WindowHWND m_window;
-};
-
-
-template<typename Context>
-GlfwWindow<Context>::GlfwWindow(const WindowConfiguration& config) : Thyme::Window{ config } {
-    TH_API_LOG_DEBUG("Create window with parameters: width = {}, height = {}, name = {}",
-                     config.width,
-                     config.height,
-                     config.name);
-    if (glfwInit() == GLFW_FALSE) {
-        auto message = "Failed to initialize GLFW!";
-        TH_API_LOG_ERROR(message);
-        glfwTerminate();
-        throw std::runtime_error(message);
-    }
-    Context::init();
-    m_window = WindowHWND(glfwCreateWindow(config.width, config.height, config.name.c_str(), nullptr, nullptr),
-                          [config](GLFWwindow* window) {
-                              TH_API_LOG_DEBUG("destroying window with parameters: width = {}, height = {}, name = {}",
-                                               config.width,
-                                               config.height,
-                                               config.name)
-                              if (window != nullptr) {
-                                  glfwDestroyWindow(window);
-                              }
-                          });
-};
-
-using VulkanGlfwWindow = GlfwWindow<GlfwVulkanContext>;
 
 };// namespace Thyme
